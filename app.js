65318e264b28d56d3864df945409d61f0b3da5a0
NguyenMinhKhoi <nguyenminhkhoii275@gmail.com>
2026-03-23 01:06:24 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,21 +1,7 @@
-function login() {
-  localStorage.setItem("login", "true");
-  localStorage.setItem("name", "NguyenKhoi Dev");
-  location.reload();
+function showContact() {
+  document.getElementById("popup").style.display = "block";
 }
 
-function logout() {
-  localStorage.removeItem("login");
-  location.reload();
-}
-
-if (localStorage.getItem("login")) {
-  document.getElementById("login-box").style.display = "none";
-  document.getElementById("app").style.display = "block";
-
-  document.getElementById("name").innerText =
-    localStorage.getItem("name");
-
-  // fake tiền
-  document.getElementById("money").innerText = "1000";
+function closePopup() {
+  document.getElementById("popup").style.display = "none";
 }
